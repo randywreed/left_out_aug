5a9c7a949c65b2975e812671d62193c41c34f4d8
randy reed <reedrw@appstate.edu>
2021-01-23 15:47:58 -0500

    add word embedding setup

---
+++ word_emb_setup.py
@@ -0,0 +1,6 @@
+#word_emb_setup.py
+from nlpaug.util.file.download import DownloadUtil
+DownloadUtil.download_word2vec(dest_dir='.') # Download word2vec model
+DownloadUtil.download_glove(model_name='glove.6B', dest_dir='.') # Download GloVe model
+DownloadUtil.download_fasttext(model_name='wiki-news-300d-1M', dest_dir='.') # Download fasttext model
+
